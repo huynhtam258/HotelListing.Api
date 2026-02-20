20fbcf71fc8fd282833ce630792f4f57b15856ab
liam nguyen <long.ndh@nscsoftware.com>
2026-02-20 20:47:00 +0700

    First commit

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+		public HotelListingDbContext(DbContextOptions<HotelListingDbContext> options) : base(options) { }
+
+		public DbSet<Country> Countries { get; set; }
+		public DbSet<Hotel> Hotels { get; set; }
+	}
+}
