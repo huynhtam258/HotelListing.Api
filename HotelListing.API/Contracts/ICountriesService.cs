6ce8d5b4a2b71465b2959c0a211e1f9d4ef3ab44
liam nguyen <long.ndh@nscsoftware.com>
2026-02-23 22:25:54 +0700

    handle servcie, auto mapper and result format

---
+++ HotelListing.API/Contracts/ICountriesService.cs
@@ -0,0 +1,16 @@
+﻿using HotelListing.API.DTOs.Country;
+using HotelListing.API.Results;
+
+namespace HotelListing.API.Contracts;
+
+public interface ICountriesService
+{
+    Task<bool> CountryExistsAsync(int id);
+    Task<bool> CountryExistsAsync(string name);
+    Task<Result<GetCountryDto>> CreateCountryAsync(CreateCountryDto createDto);
+    Task<Result> DeleteCountryAsync(int id);
+    Task<Result<IEnumerable<GetCountriesDto>>> GetCountriesAsync();
+    Task<Result<GetCountryDto>> GetCountryAsync(int id);
+    Task<Result> UpdateCountryAsync(int id, UpdateCountryDto updateDto);
+
+}
