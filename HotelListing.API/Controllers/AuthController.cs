1a9ac19846f9d6e336dcedc5cafb3bd6438bb06d
liam nguyen <long.ndh@nscsoftware.com>
2026-02-24 11:57:03 +0700

    Add custom authentication endpoints

---
+++ HotelListing.API/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using HotelListing.Api.Controllers;
+using HotelListing.API.Constants;
+using HotelListing.API.Data;
+using HotelListing.API.DTOs.Auth;
+using HotelListing.API.Results;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelListing.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(UserManager<ApplicationUser> userManager): BaseApiController
+{
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterUserDto registerUserDto)
+    {
+        var user = new ApplicationUser
+        {
+            Email = registerUserDto.Email,
+            FristName = registerUserDto.FirstName,
+            LastName = registerUserDto.LastName,
+            UserName = registerUserDto.Email
+        };
+        var result = await userManager.CreateAsync(user, registerUserDto.Password);
+
+        if (!result.Succeeded)
+        {
+            var errors = result.Errors.Select(e => new Error(ErrorCodes.BadRequest, e.Description)).ToArray();
+            return MapErrorsToResponse(errors);
+        }
+
+        // Optional: Send confirmation Email
+        return Ok();
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginUserDto loginUserDto)
+    {
+        var user = await userManager.FindByEmailAsync(loginUserDto.Email);
+
+        if (user == null) {
+            return Unauthorized(new { message = "Invalid Credentials" });
+        }
+
+        var isPasswordValid = await userManager.CheckPasswordAsync(user, loginUserDto.Password);
+        if (!isPasswordValid)
+        {
+            return Unauthorized(new { message = "Invalid Credentials" });
+        }
+
+        return Ok();
+    }
+}
---
+++ HotelListing.API/DTOs/Auth/LoginUserDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.DTOs.Auth;
+
+public class LoginUserDto
+{
+
+    [Required, EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
