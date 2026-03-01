bb3b780b55289aeb8f94ee55d894661b63a6242d
liam nguyen <long.ndh@nscsoftware.com>
2026-03-01 11:29:56 +0700

    ApiKey data model

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -4,13 +4,19 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Data;
 
-public class HotelListingDbContext : IdentityDbContext<ApplicationUser>
+public class HotelListingDbContext(DbContextOptions<HotelListingDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-	public HotelListingDbContext(DbContextOptions<HotelListingDbContext> options) : base(options) 
-	{
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<ApiKey> ApiKeys { get; set; }
 
-	}
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
 
-	public DbSet<Country> Countries { get; set; }
-	public DbSet<Hotel> Hotels { get; set; }
+        builder.Entity<ApiKey>(b =>
+        {
+            b.HasIndex(k => k.Key).IsUnique();
+        });
+    }
 }
